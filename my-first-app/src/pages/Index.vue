c808faf25b0007dc788c3dd499753191f8428de3
Nadeeya Bunya <5910110167@psu.ac.th>
2021-01-16 02:56:08 +0700

    commit1

---
+++ my-first-app/src/pages/Index.vue
@@ -0,0 +1,14 @@
+<template>
+  <q-page class="flex flex-center">
+    <img
+      alt="Quasar logo"
+      src="https://cdn.quasar.dev/img/mountains.jpg"
+    >
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: 'PageIndex'
+}
+</script>
